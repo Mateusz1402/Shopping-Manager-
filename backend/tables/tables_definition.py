66ab568a247b6ba35eac64bf3ef61329d0217f3a
Mateusz <mateusz.sabat38@gmail.com>
2026-06-27 17:51:57 +0200

    feat: initial commit

---
+++ backend/tables/tables_definition.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean, Float
+
+string_db = "postgresql://postgres:postgres@localhost:5432/grocery_shopping_db"
+engine = create_engine(string_db)
+Base = declarative_base()
+
+#categories table
+class Categories(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    category_name = Column(String, nullable=False)
+
+
+#products table
+class Products(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    is_ordered = Column(Boolean, nullable=False, default=False)
+
+Base.metadata.create_all(engine)
+
+
+
+
