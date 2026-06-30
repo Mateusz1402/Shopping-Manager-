e2f035a6dae6ae89e483475ba6c02f25f8082372
Mateusz <mateusz.sabat38@gmail.com>
2026-06-30 16:56:04 +0200

    feat: users table

--- backend/tables/tables_definition.py
+++ backend/tables/tables_definition.py
@@ -6,20 +6,31 @@ string_db = "postgresql://postgres:postgres@localhost:5432/grocery_shopping_db"
 engine = create_engine(string_db)
 Base = declarative_base()
 
-#categories table
+
+# categories table
 class Categories(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
     category_name = Column(String, nullable=False, unique=True)
 
 
-#products table
+# products table
 class Products(Base):
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
     is_ordered = Column(Boolean, nullable=False, default=False)
+
+
+# users table
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="guest")
+
 
 Base.metadata.create_all(engine)
 
